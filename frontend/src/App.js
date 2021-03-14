1b255af0a31806432b49e7f6433b8f0d0bc7d1dc
Jean Matheus Vassalo Ferreira <jeanvassaloferreira@gmail.com>
2021-03-14 11:33:55 -0300

    Initiating frontend and installing dependencies
    I initiated the frontend folder with 'npx create-react-app frontend' and after that I installed axios, @material-ui/core and @material-ui/data-grid

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          React Start
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
